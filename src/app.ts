0c28599e883bba1ce6f5f4db611d76767a83289a
itsmeAYUSH <ayushksagar14@gmail.com>
2026-03-28 13:27:42 +0530

    setup

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("server is running on port", PORT);
+});
+
+export default app;
